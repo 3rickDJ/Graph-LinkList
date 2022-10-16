c0ab88587338c2fce37e87faa39c03b728746c3a
3rick <erick.dejesus1@alumno.buap.mx>
2022-10-15 22:36:56 -0500

    Tipo de dato lista (clase Grafo)

---
+++ src/List.cpp
@@ -0,0 +1,113 @@
+#pragma once
+#include "List.h"
+#include "NodeGraph.h"
+#include "NodeAdj.h"
+using std::cout;
+List::List() {
+    // inicializar el apuntador en nullo
+    pList = nullptr;
+}
+void List::insertarInicio(int x) {
+    // variable auxiliar
+    NodeGraph *p = new NodeGraph();
+    // asignar valor al nodo
+    p->value = x;
+    // linkear nodo a la lista
+    p->next = pList;
+    pList = p;
+}
+void List::insertarFinal(int x) {
+    // si esta vacio no podemos acceder a p->next->next
+    // nos da un error
+    if (listaVacia()) {
+        // por si es el primer elemento
+        insertarInicio(x);
+    } else {
+        NodeGraph *p = pList;
+        // avanzar al siguiente nodo mientras no sea el ultimo
+        while (p->next != 0) {
+            p = p->next;
+        }
+        // encontrando el ultimo nodo
+        // obtenemos espacio para el siguiente nodo
+        p->next = new NodeGraph();
+        // ultimo elemento apunta a nulo
+        p->next->next = 0;
+        // asignar el valor del ultimo elemento
+        p->next->value = x;
+    }
+}
+void List::imprimir() {
+    NodeGraph::printList(pList);
+}
+void List::imprimirAdj(int elm) {
+    NodeGraph *p = NodeGraph::find(pList,elm);
+    // recorrer cada nodo mientras no apuntemos a nulo
+    NodeAdj::printList(p->adj);
+}
+/* void List::eliminarPrimero() { */
+/*     if (listaVacia()) { */
+/*         // verificar si esta vacia */
+/*         std::cout << "No hay elementos.\n"; */
+/*     } else { */
+/*         NodeGraph *p = pList; */
+/*         // apuntar al siguiente elemento */
+/*         pList = pList->next; */
+/*         // eliminar el primer elemento */
+/*         free(p); */
+/*     } */
+/* } */
+/* void List::eliminarUltimo() { */
+/*     NodeGraph *p = pList; */
+/*     // si esta vacio no podemos acceder a p->next->next */
+/*     // nos da un error */
+/*     if (listaVacia()) { */
+/*         // si esta vacia mandar mensaje */
+/*         std::cout << "No hay elementos.\n"; */
+/*     } else if (p->next == 0) { */
+/*         free(p); */
+/*         pList = 0; */
+/*     } else { */
+/*         // iterar hasta llegar al penultimo nodo */
+/*         while (p->next->next != 0) { */
+/*             // siguiente nodo */
+/*             p = p->next; */
+/*         } */
+/*         // liberar el ultimo nodo */
+/*         free(p->next); */
+/*         // ultimo nodo apuntando a null */
+/*         p->next = 0; */
+/*     } */
+/* } */
+bool List::listaVacia() {
+    // lista no tiene elementos si apunta a nullo
+    return pList == nullptr;
+}
+
+bool List::isAdj(int elmA, int elm) {
+    NodeGraph *p = NodeGraph::find(pList,elmA);
+    // verificar si el nodo del grafo es adjacente a elm
+    return p->isAdj(elm);
+}
+void List::addAdjacency(int elmA, int elm) {
+    NodeGraph *p = NodeGraph::find(pList,elmA);
+    if (p == 0) {
+        cout << "Nodo " << elmA << " no encontrado\n";
+    } else {
+        addAdj(p, elm);
+    }
+}
+void List::addAdj(NodeGraph *nodoGrafo, int elm) {
+    NodeAdj *p = nodoGrafo->adj;
+    NodeAdj *nuevoAdj = new NodeAdj();
+    nuevoAdj->value = elm;
+    //linkear nodo a lista adj
+    if(p==nullptr){
+        p=nuevoAdj;
+    }else{
+        while(p!=nullptr){
+            p=p->next;
+        }
+        p=nuevoAdj;
+    }
+}
--- src/List.h
+++ src/List.h
@@ -3,153 +3,22 @@
 #include "NodeGraph.h"
 #include <cstdlib>
 #include <iostream>
-using std::cout;
 class List {
     NodeGraph *pList;
 
+    void insertarInicio(int x);
+    void insertarFinal(int x);
+    void eliminarPrimero();
+    void eliminarUltimo();
+    bool listaVacia();
   public:
-    List() {
-        // inicializar el apuntador en nullo
-        pList = nullptr;
-    }
-    NodeGraph* find(int elm) {
-        NodeGraph *p=pList;
-        while (p != 0 && (p->value != elm)) {
-            p = p->next;
-        }
-        if (p == 0) {
-            return nullptr;
-        }
-        // no es nulo --> encontro el elemento
-        return p;
-    }
-    void insertarInicio(int x) {
-        // variable auxiliar
-        NodeGraph *p;
-        p = (NodeGraph *)malloc(sizeof(NodeGraph));
-        // asignar valor al nodo
-        p->value = x;
-        // linkear nodo a la lista
-        p->next = pList;
-        pList = p;
-    }
-    void insertarFinal(int x) {
-        // si esta vacio no podemos acceder a p->next->next
-        // nos da un error
-        if (listaVacia()) {
-            // por si es el primer elemento
-            insertarInicio(x);
-        } else {
-            NodeGraph *p = pList;
-            // avanzar al siguiente nodo mientras no sea el ultimo
-            while (p->next != 0) {
-                p = p->next;
-            }
-            // encontrando el ultimo nodo
-            // obtenemos espacio para el siguiente nodo
-            p->next = (NodeGraph *)malloc(sizeof(NodeGraph));
-            // ultimo elemento apunta a nulo
-            p->next->next = 0;
-            // asignar el valor del ultimo elemento
-            p->next->value = x;
-        }
-    }
-    void imprimir() {
-       NodeGraph  *p = pList;
-        // recorrer cada nodo mientras no apuntemos a nulo
-        while (p != 0) {
-            // imprimir el valor del i-esimo nodo
-            std::cout << p->value << ", ";
-            // pasar al siguiente nodo
-            p = p->next;
-        }
-        // imprimir enter final
-        std::cout << "\n";
-    }
-    void imprimirAdj(int elm) {
-        NodeGraph *p = find(elm);
-        // recorrer cada nodo mientras no apuntemos a nulo
-        cout <<"Nodos adyacentes a "<<elm<<" son:\n";
-        while (p != 0) {
-            // imprimir el valor del i-esimo nodo
-            std::cout << p->value << ", ";
-            // pasar al siguiente nodo
-            p = p->next;
-        }
-        // imprimir enter final
-        std::cout << "\n";
-    }
-    void eliminarPrimero() {
-        if (listaVacia()) {
-            // verificar si esta vacia
-            std::cout << "No hay elementos.\n";
-        } else {
-            NodeGraph *p = pList;
-            // apuntar al siguiente elemento
-            pList = pList->next;
-            // eliminar el primer elemento
-            free(p);
-        }
-    }
-    void eliminarUltimo() {
-        NodeGraph *p = pList;
-        // si esta vacio no podemos acceder a p->next->next
-        // nos da un error
-        if (listaVacia()) {
-            // si esta vacia mandar mensaje
-            std::cout << "No hay elementos.\n";
-        } else if (p->next == 0) {
-            free(p);
-            pList = 0;
-        } else {
-            // iterar hasta llegar al penultimo nodo
-            while (p->next->next != 0) {
-                // siguiente nodo
-                p = p->next;
-            }
-            // liberar el ultimo nodo
-            free(p->next);
-            // ultimo nodo apuntando a null
-            p->next = 0;
-        }
-    }
-    bool listaVacia() {
-        // lista no tiene elementos si apunta a nullo
-        return pList == 0;
-    }
+    List();
 
-    bool isAdj(int elmA, int elm) {
-        NodeGraph *p = pList;
-        // recorrer cada nodo mientras no apuntemos a nulo o encontremos elmA
-        while (p != 0 && (p->value != elmA)) {
-            // pasar al siguiente nodo
-            p = p->next;
-        }
-        if (p == 0) {
-            return false;
-        }
-        // verificar si el nodo del grafo es adjacente a elm
-        return p->isAdj(elm);
-    }
-    void addAdjacency(int elmA, int elm) {
-        NodeGraph *p = find(elmA);
-        if (p == 0) {
-            cout << "Nodo " << elmA << " no encontrado\n";
-        } else {
-            addEndAdj(p, elm);
-        }
-    }
-    void addEndAdj(NodeGraph *nodoGrafo, int elm) {
-        NodeAdj *p = nodoGrafo->adj;
-        NodeAdj *nuevoAdj = (NodeAdj *)malloc(sizeof(NodeAdj));
-        nuevoAdj->value = elm;
-        if (p == 0) {
-            p = nuevoAdj;
-        } else {
-            while (p->next != 0) {
-                p = p->next;
-            }
-            p->next = nuevoAdj;
-        }
-    }
+    void imprimirAdj(int elm);
+    void imprimir();
+    void insertarNodo(int x);
+    bool isAdj(int elmA, int elm);
+    void addAdjacency(int elmA, int elm);
+    void addAdj(NodeGraph *nodoGrafo, int elm);
+    /* void addEndAdj(NodeGraph *nodoGrafo, int elm); */
 };
