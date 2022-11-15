32608cb11658d93575dcb58935fcc50038ef18e4
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-14 19:35:33 -0600

    add: implementation of constructor and insertarNodo

--- src/BinTree.cpp
+++ src/BinTree.cpp
@@ -1,1 +1,22 @@
 #include "BinTree.h"
+
+BinTree::BinTree() {
+    //Asignacion de valor a pTree
+    pTree = nullptr;
+}
+
+//insertar el nodo en el arbol
+void BinTree::insertarNodo(int elm, NodoTree *root) {
+    if(root== nullptr){ // Si no hay elementos en la raíz, inserta un nuevo nodo
+        root = new NodoTree(elm);
+    }else{  // Si no, inserta elementos a la derecha o a la izquierda del nodo seleccionado
+        if(elm <= root->value){ // Si el elemento es menor al del nodo, entonces se va a insertar a la izquierda
+            insertarNodo(elm, root->left);
+        }else{ // si no, se va a la derecha
+            insertarNodo(elm, root->right);
+        }
+    }
+}
+
+
+
