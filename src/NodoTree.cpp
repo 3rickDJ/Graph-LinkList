58d3fd8bf24991668e0b08fdabb75c8957cdbb1d
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-14 19:31:19 -0600

    add: constructors for class NodoTree

--- src/NodoTree.cpp
+++ src/NodoTree.cpp
@@ -1,1 +1,19 @@
 #include "NodoTree.h"
+
+using namespace std;
+//Este constructor no tiene parametros para poner los valores por default
+NodoTree::NodoTree() {
+    this->value = -1;
+    visited = 0;
+    left = nullptr;
+    right = nullptr;
+
+}
+//Constructor para cuando se crea un nodo con un valor digitado por el usuario
+NodoTree::NodoTree(int value) {
+    this->value = value;
+    visited  = 0;
+    left = nullptr;
+    right = nullptr;
+}
+
